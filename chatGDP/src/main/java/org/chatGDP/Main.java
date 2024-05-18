53415884c5e29065ba9b595eb949d21db7ad215f
Petra <petst@itu.dk>
2024-05-18 14:54:50 +0200

    add some connection to db but needs improvments

--- chatGDP/src/main/java/org/chatGDP/Main.java
+++ chatGDP/src/main/java/org/chatGDP/Main.java
@@ -1,7 +1,25 @@
 package org.chatGDP;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Hello world!");
+//        try (var connection =  DB.connect()){
+//            System.out.println("Connected to the PostgreSQL database.");
+//        } catch (SQLException e) {
+//            System.err.println(e.getMessage());
+//        }
+        String url = "jdbc:postgresql://localhost:5432/chatGDP";
+        Properties props = new Properties();
+        props.setProperty("user", "postgres");
+        props.setProperty("password", "root");
+        Connection conn = DriverManager.getConnection(url, props);
+
+//        String url = "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true";
+//        Connection conn = DriverManager.getConnection(url);
     }
 }
---
+++ chatGDP/src/main/java/org/chatGDP/models/User.java
@@ -0,0 +1,56 @@
+package org.chatGDP.models;
+
+public class User {
+    private String id;
+    public String name;
+    public String lastName;
+    public String createDate;
+    public int isActive;
+
+    public User(String name, String lastName, String createDate, int isActive) {
+        this.name = name;
+        this.lastName = lastName;
+        this.createDate = createDate;
+        this.isActive = isActive;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public int getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(int isActive) {
+        this.isActive = isActive;
+    }
+}
