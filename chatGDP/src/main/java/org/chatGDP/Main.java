75c697c83a2a10c7f6a99b16cee369124cc2729e
tzoan218 <142490646+tzoan218@users.noreply.github.com>
2024-11-23 12:21:10 +0100

    sto esvisa

--- chatGDP/src/main/java/org/chatGDP/Main.java
+++ chatGDP/src/main/java/org/chatGDP/Main.java
@@ -5,7 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Goodbye world");
         System.out.println("Hello again");
-        System.out.println("Hello again");
 
     }
 }
